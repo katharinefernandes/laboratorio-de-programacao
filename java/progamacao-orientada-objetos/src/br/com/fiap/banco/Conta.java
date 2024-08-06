3eef30a46affd2e6bac630b3ba3b3197c91d9509
Katharine Fernandes <70667492+katharinefernandes@users.noreply.github.com>
2024-08-05 22:22:36 -0300

    :sparkles: Arquivos aula 1 - Introdução

--- java/progamacao-orientada-objetos/src/br/com/fiap/banco/Conta.java
+++ java/progamacao-orientada-objetos/src/br/com/fiap/banco/Conta.java
@@ -1,7 +1,7 @@
 package br.com.fiap.banco;
 
 public class Conta {
-	
+
 	// static para quando não quero instanciar um objeto
 
 	// nível mais restritivo (só visto dentro da própria classe)
@@ -9,23 +9,10 @@ public class Conta {
 	private String numConta;
 	private double saldo;
 	// BigDecimal para cálculos financeiros (arredondamento mais bem definido)
-	// private Cliente cliente;
+	private Cliente cliente;
 
-	public void deposito(double valor) {
-		// referência o atributo da própria classe (como se fosse o global)
-		// é uma boa prática
-		this.saldo += valor;
-	}
-	
-	public boolean saque(double valor) {
-		if (this.saldo >= valor) {
-			this.saldo -= valor;
-			return true;
-		}
-		return false;
-	}
-	
-	// encapsulamento para acessar os atributos, para implementar o JavaBean é necessário criar os getters e setters
+	// encapsulamento para acessar os atributos, para implementar o JavaBean é
+	// necessário criar os getters e setters
 
 	public String getNumAgencia() {
 		return numAgencia;
@@ -47,5 +34,39 @@ public class Conta {
 		return saldo;
 	}
 
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public void deposito(double valor) {
+		// referência o atributo da própria classe (como se fosse o global)
+		// é uma boa prática
+		this.saldo += valor;
+	}
+
+	public boolean saque(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean transferencia(String numConta, String numAgencia, double valor) {
+
+		if (!this.numConta.equals(numConta) || !this.numAgencia.equals(numAgencia)) {
+			if (this.saldo >= valor) {
+				this.saldo -= valor;
+				return true;
+			}
+		}
+
+		return false;
+
+	}
 
 }
